9d7c880c4cf65b87c7d5af4a2977c50d518a60e9
André Souza <andrestephano@gmail.com>
2019-12-19 20:11:41 -0300

    feature: adds GroupCollect.Report.ProductParser

---
+++ lib/syncfile/product_parser.ex
@@ -0,0 +1,50 @@
+NimbleCSV.define(CSV, separator: "|")
+
+defmodule GroupCollect.Report.ProductParser do
+  @moduledoc """
+  Parse the Product CSV containing:
+
+  PART_NUMBER = identifier for a product
+  BRANCH_ID = which kloeckner branch produces the product (TUC = tucson, CIN = cincinnati)
+  PART_PRICE = price in USD
+  SHORT_DESC = short description text about the product
+
+  The CSV for the product uses `|` as the delimiter
+  """
+
+  def load_csv() do
+    "tmp/data.csv"
+    |> File.read!()
+    |> process_csv()
+    |> to_map
+  end
+
+  @doc """
+  Receives the raw CSV content and format to a list of maps
+
+  ## Example
+      iex> content = "PART_NUMBER|BRANCH_ID|PART_PRICE|SHORT_DESC\n0121F00548|..."
+      iex> GroupCollect.Report.ProductParser.process_csv(content)
+      [
+        %{
+          "BRANCH_ID" => "TUC",
+          "PART_NUMBER" => "0121F00548",
+          "PART_PRICE" => "3.14",
+          "SHORT_DESC" => "GALV x FAB x 0121F00548 x 16093 x .026 x 29.88 x 17.56"
+        },
+        %{...},
+        ...
+      ]
+
+  """
+  def process_csv(csv_string) do
+    csv_string
+    |> CSV.parse_string(skip_headers: false)
+    |> to_map
+  end
+
+  defp to_map(parsed_csv) do
+    [header | body] = parsed_csv
+    body |> Enum.map(fn row -> Enum.zip(header, row) |> Enum.into(%{}) end)
+  end
+end
--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule Syncfile.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:nimble_csv, "~> 0.6"}
     ]
   end
 
